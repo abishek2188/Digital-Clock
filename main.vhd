7e7add618425849e1b18069f31cf30a8a07478cc
Abishek <abishek.50c@gmail.com>
2020-12-15 23:32:14 +0530

    made n modulo counter

--- main.vhd
+++ main.vhd
@@ -13,3 +13,25 @@ ENTITY main IS
         display_decimal :   OUT std_logic_vector(3 DOWNTO 0);
     );
 END main;
+
+ARCHITECTURE logic OF main is
+    SIGNAL clk_1        :   std_logic := '0';
+    SIGNAL count        :   std_logic_vector(13 DOWNTO 0) := "00000000000000";
+BEGIN
+    PROCESS(clk)
+        BEGIN
+            IF (rising_edge(clk)) THEN
+                IF (count = "01001110000111") THEN 
+                    count <= "00000000000000";
+                    IF (clk_1 = '0') THEN
+                        clk_1 <= '1';
+                    ELSE
+                        clk_1 <= '0';
+                    END IF;
+                ELSE 
+                    count <= count + 1;
+                END IF;
+            END IF;
+        END PROCESS;
+    
+        
