b0bae492feae007ab370c439ac618ec45b8aa44f
Abishek <abishek.50c@gmail.com>
2020-12-15 22:31:45 +0530

    capitalized

--- main.vhd
+++ main.vhd
@@ -1,9 +1,9 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.all;
+USE IEEE.numeric_std.all;
 
-entity main is
-    port (
+ENTITY main IS
+    PORT (
         clk             :   IN  std_logic;
         b1              :   IN  std_logic;
         b2              :   IN  std_logic;
@@ -12,4 +12,4 @@ entity main is
         display_7seg    :   OUT std_logic_vector(27 DOWNTO 0);
         display_decimal :   OUT std_logic_vector(3 DOWNTO 0);
     );
-end main;
+END main;
