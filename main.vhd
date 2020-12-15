e21e6a7e449bb4a83d88b996548ccdbf4360c259
Abishek <abishek.50c@gmail.com>
2020-12-16 02:06:10 +0530

    coded tracking of time

--- main.vhd
+++ main.vhd
@@ -5,10 +5,7 @@ USE IEEE.numeric_std.all;
 ENTITY main IS
     PORT (
         clk             :   IN  std_logic;
-        b1              :   IN  std_logic;
-        b2              :   IN  std_logic;
-        b3              :   IN  std_logic;
-        b4              :   IN  std_logic;
+        b1, b2, b3, b4  :   IN  std_logic;
         display_7seg    :   OUT std_logic_vector(27 DOWNTO 0);
         display_decimal :   OUT std_logic_vector(3 DOWNTO 0);
     );
@@ -28,12 +25,42 @@ ARCHITECTURE logic OF main is
             clk_1ms     :       OUT std_logic := '0';
         );
     END COMPONENT;
+    TYPE state_type IS (time_hour_min, time_min_sec, change_hour, change_min);
+    TYPE state_type1 IS (display_time, change_time);
+    SIGNAL state1                                               :   state_type1;
+    SIGNAL state                                                :   state_type;
+    SIGNAL clk_1ms                                              :   std_logic := '0';
+    SIGNAL clk_1s                                               :   std_logic := '0';
+    SIGNAL minute, temp_minute, second, temp_second, hour, temp_hour     :   std_logic_vector(6 DOWNTO 0) := '0';
 
-    SIGNAL clk_1ms          :   std_logic := '0';
-    SIGNAL clk_1s           :   std_logic := '0';
 BEGIN
     create_1s_clock: time_clk_1s port map (clk, clk_1s);
     create_1ms_clock: time_clk_1ms port map (clk, clk_1ms);
+    PROCESS(clk_1s)
+        BEGIN
+            CASE state1 IS
+                WHEN display_time =>
+                    IF rising_edge(clk_1s) THEN
+                        second <= second + 1;
+                        IF (second = "111100") THEN
+                            second <= '0';
+                            minute <= minute + 1;
+                            IF (minute = "111100") THEN
+                                minute <= '0';
+                                hour <= hour + 1;
+                                IF (hour = "011000") THEN
+                                    hour <= '0';
+                                END IF;
+                            END IF;
+                        END IF;
+                    END IF;
+                WHEN change_time =>
+            END CASE;
+        END PROCESS;
+                    
+        
+                
+
             
 
         
